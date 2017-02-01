5f4c231fbeb5cee26d7ca78747e10df45f2b866c
Jesper jumziey Vesterberg <jumziey@gmail.com>
2017-02-01 18:40:02 +0100

    Sneaky script most def done

--- testreq.py
+++ testreq.py
@@ -1,5 +1,6 @@
 import urllib.request
 import time
+
 def getResponseCode(url):
     try:
         conn = urllib.request.urlopen(url)
@@ -7,6 +8,29 @@ def getResponseCode(url):
     except urllib.error.HTTPError:
         return "0"
 
+def tests(years, months, days, endings):
+    filenames = [""];
+    for y in range(years):
+        for m in range(months):
+            for d in range(days):
+                if y<10:
+                    year = "0" + str(y)
+                else:
+                    year = str(y)
+
+                if (m+1)<10:
+                    month = "0" + str(m+1)
+                else:
+                    month = str(m+1)
+
+                if(d+1)<10:
+                    day = "0" + str(d+1)
+                else:
+                    day = str(d+1)
+
+                for ending in endings:
+                    filenames.append("t"+year+month+day + "." + ending)
+    return filenames
 
 years = 16
 months = 12-1 #remember +1
@@ -17,34 +41,12 @@ endings = [
     "txt",
     "tex"
 ]
-
-tests = [""];
-for y in range(years):
-    for m in range(months):
-        for d in range(days):
-            if y<10:
-                year = "0" + str(y)
-            else:
-                year = str(y)
-
-            if (m+1)<10:
-                month = "0" + str(m+1)
-            else:
-                month = str(m+1)
-
-            if(d+1)<10:
-                day = "0" + str(d+1)
-            else:
-                day = str(d+1)
-
-            for ending in endings:
-                tests.append("t"+year+month+day + "." + ending)
-
-for test in tests:
+for test in tests(years, months, days, endings):
     print(test)
 
 testurl = ["http://www.google.se", "https://docs.python.org/3.0/library/random.html", "http://google.se/aoeuhtnasoeuhtnsaoeu"]
 for url in testurl:
     code = getResponseCode(url)
-    if code != 404:
+    print(code);
+    if code == 200:
         print(url +" works!");
